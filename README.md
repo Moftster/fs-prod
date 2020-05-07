26580a541afc151e18edf1134f4ccb79362f04bd
David Moftakhar <david@sellmysnaps.com>
2020-05-07 13:12:57 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# freshstart
+Website for FreshStart Psychotherapty - a London-based Community Interest Charity offering psychotherapy and counselling services to clients and therapists.
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homepage');
+});
+
+
+Route::view('contact', 'contact');
+Route::post('contactsubmission', 'ContactController@submitContactForm');
+
+Route::view('ourpeople', 'ourpeople');
+
+Route::view('gdpr', 'gdpr');
+
+Route::view('events', 'events');
+
+Route::view('about', 'about');
+
+Route::view('fees', 'fees');
+
+Auth::routes(['register' => false]);
+
